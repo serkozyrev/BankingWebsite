db1cfca983999f9159ba5540262a7c2fb9575030
Sergey Kozyrev <serega3306@mail.ru>
2022-06-12 11:46:59 -0400

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 
 import "./App.css";
-import ExpensesWebsite from "./components/Website/ExpensesWebsite";
-import EditRecord from "./components/Website/EditRecord";
-import Analytics from "./components/Website/Analytics/Analytics";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+const ExpensesWebsite = React.lazy(() =>
+  import("./components/Website/ExpensesWebsite")
+);
+const EditRecord = React.lazy(() => import("./components/Website/EditRecord"));
+const Analytics = React.lazy(() =>
+  import("./components/Website/Analytics/Analytics")
+);
 
 const App = () => {
   return (
-    <div>
+    <Suspense
+      fallback={
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      }
+    >
       <Header />
       <Routes>
         <Route exact path="/" element={<ExpensesWebsite />} />
         <Route path="/edit/:rid&:type" element={<EditRecord />} />
         <Route path="analysis" element={<Analytics />} />
       </Routes>
-    </div>
+    </Suspense>
   );
 };
 
